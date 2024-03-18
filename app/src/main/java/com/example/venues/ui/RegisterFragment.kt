9d788c76bd01245f88af5895045b7e667ea792c7
George Samuel <georges012441@gmail.com>
2024-03-18 13:03:22 +0200

    add register ui view

--- app/src/main/java/com/example/venues/ui/RegisterFragment.kt
+++ app/src/main/java/com/example/venues/ui/RegisterFragment.kt
@@ -6,55 +6,47 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.venues.R
+import com.example.venues.databinding.FragmentRegisterBinding
+import com.google.android.material.datepicker.MaterialDatePicker
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegisterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
+    private var selectedBirthDay = MaterialDatePicker.todayInUtcMilliseconds()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+
+        listeners()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun listeners() {
+        binding.apply {
+            btnDate.setOnClickListener {
+                val picker =
+                    MaterialDatePicker.Builder.datePicker()
+                        .setTitleText(getString(R.string.select_date))
+                        .setSelection(selectedBirthDay)
+                        .build()
+
+                picker.show(requireActivity().supportFragmentManager, RegisterFragment::class.simpleName)
+
+                picker.addOnPositiveButtonClickListener {
+                    selectedBirthDay = it
                 }
             }
+
+            btnRegister.setOnClickListener {
+
+            }
+        }
     }
 }
